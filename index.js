82e37e7679f0730bc533b058607fa70a5a99ab24
Raphael Amorim <rapha850@gmail.com>
2017-12-12 00:19:48 -0200

    0.0.6

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const { execSync } = require('child_process');
+const { spawn } = require('child_process');
 
 function run(commands, path) {
   if (!commands.length) {
     return false;
   }
 
-  let command = 'node ./bin/ares-' + commands[0] + '.js'
+  let command = __dirname + '/bin/ares-' + commands[0] + '.js'
   if (commands.length >= 2) {
     commands.shift()
     command += ' ' + commands.join(' ');
   }
 
-  const subject = execSync(command, {cwd: process.cwd()});
+  console.log(command, path)
+  const subject = spawn('node', [command], {cwd: path});
   return subject;
 }
 
